65bd2dd16fc8f4165e545a090d994696c5e50574
akarapunzl <Samanthagduffy@gmail.com>
2024-02-29 18:11:20 -0600

    updating code

--- EmployeeSQL/Employee.sql
+++ EmployeeSQL/Employee.sql
@@ -43,8 +43,6 @@ ON employees.emp_no = salary.emp_no;
 SELECT first_name, last_name, hire_date FROM employees
 WHERE EXTRACT(YEAR FROM hire_date) = 1986;
 
-SELECT * FROM dept_manager
-
 --List the manager of each department along with their department number, department name, employee number, last name, and first name.
 SELECT dept_manager.emp_no, dept_manager.dept_no, departments.dept_name, employees.last_name, employees.first_name
 FROM dept_manager
